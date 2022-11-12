1c4e94290c8e268f7c03238dcc62d01cb1045bdd
rhatiro <roberto.nishiyama@yahoo.com.br>
2022-11-12 03:47:35 -0300

    atualizacao da aula 3

---
+++ pages/video.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { ColorModeContext } from "../src/components/Menu/components/ColorMode";
+
+export default function Video() {
+    const contexto = React.useContext(ColorModeContext);
+
+    return (
+        <div>
+            Vídeo!
+            {contexto.mode}
+            <button onClick={() => contexto.toggleMode()}>
+                Trocar modo
+            </button>
+        </div>
+    )
+}
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
-import Search from "./Search";
+import DarkModeSwitch from "./components/DarkModeSwitch";
+import Search from "./components/Search";
 
 const StyledMenu = styled.header`
   display: flex;
@@ -32,6 +33,7 @@ export default function Menu({ valorDoFiltro, setValorDoFiltro }) {
         <Logo />
       </div>
       <Search valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
+      <DarkModeSwitch />
     </StyledMenu>
   );
 }
